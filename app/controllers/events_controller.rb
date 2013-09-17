d49cb90fbd86144508cd2b065ad3b2cf5af5fafd
Diego Ortiz-Monasterio <dortizmonasterio@gmail.com>
2013-09-17 11:38:10 -0700

    Color coding

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -6,7 +6,7 @@ class EventsController < ApplicationController
   # GET /events.json
   def index
     @published_events = Event.where("status = :status AND end_time >= :date",
-                                    {status: "Publish", date: Time.now})
+                                    {status: "Publish", date: DateTime.now})
     @draft_events = Event.where(status: "Draft")
 
     respond_to do |format|
