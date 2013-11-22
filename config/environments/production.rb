9b759a67427d5ab57e8a734e19d49db8bc5a938e
Diego Ortiz-Monasterio <dortizmonasterio@gmail.com>
2013-11-22 14:36:57 -0700

    Adding default host for mailer

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -51,6 +51,7 @@ Asudisc::Application.configure do
 
   # Disable delivery errors, bad email addresses will be ignored
   # config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.default_url_options = { host: 'asudisc.org' }
 
   # Enable threaded mode
   # config.threadsafe!
