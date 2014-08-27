f7db082db84d083972ddd9b30ad72cd98216dc41
Diego Ortiz-Monasterio <dortizmonasterio@gmail.com>
2014-08-26 22:52:29 -0500

    Clean up attendance report

--- app/assets/javascripts/reports.js
+++ app/assets/javascripts/reports.js
@@ -1,6 +1,6 @@
 $(document).ready(function() {
-  $("div.well").hide();
-  $(".btn-warning").each(function() {
+  $("div.attendance-report div.well").hide();
+  $("div.attendance-report .btn-warning").each(function() {
     $(this).click(function() {
       $(this).next().next().toggle();
     });
